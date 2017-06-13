ac12c552a7b2e6aa2336e015ead5dab941257e46
Dane Schneider <dane.schneider@gmail.com>
2017-06-13 12:51:58 -0400

    fix for spring vars issue

--- lib/envkey/core.rb
+++ lib/envkey/core.rb
@@ -1,4 +1,4 @@
-require 'ffi'
+  require 'ffi'
 require 'dotenv'
 require 'json'
 require 'set'
--- lib/envkey/rails.rb
+++ lib/envkey/rails.rb
@@ -9,7 +9,7 @@ module Envkey
         ts = Time.now
         overload_dotenv_vars, overload_envkey_vars = Envkey::Core.load_env
         Spring.after_fork do
-          Envkey::Core.load_env(ts, overload_dotenv_vars, overload_envkey_vars)
+          Envkey::Core.load_env(ts, (overload_dotenv_vars || []), (overload_envkey_vars || []))
         end
       rescue LoadError
         Envkey::Core.load_env
