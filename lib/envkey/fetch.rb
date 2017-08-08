704e608f457664efb1ca371e1832c3da9bda0cff
Dane Schneider <dane.schneider@gmail.com>
2017-08-08 02:42:42 -0400

    only cache in dev/test

--- lib/envkey/fetch.rb
+++ lib/envkey/fetch.rb
@@ -5,7 +5,13 @@ module Envkey::Fetch
 
   def self.fetch_env key
     fetch_env_path = Envkey::Platform.fetch_env_path
-    `#{fetch_env_path} #{key} --cache`
+    `#{fetch_env_path} #{key}#{is_dev ? ' --cache' : ''}`
+  end
+
+  def self.is_dev
+    dev_vals = %w(development test)
+    dev_vals.include?(ENV["RAILS_ENV"]) ||
+    dev_vals.include?(ENV["RACK_ENV"])
   end
 
 end
