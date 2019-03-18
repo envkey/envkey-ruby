a7814ea4b2bd40090475baf50b630c8d299566d2
Seth Pollack <seth@sethpollack.net>
2019-03-18 15:28:37 -0400

    store original envs in env var

--- lib/envkey/core.rb
+++ lib/envkey/core.rb
@@ -4,43 +4,40 @@ require 'json'
 require 'envkey/fetch'
 
 module Envkey::Core
+  def self.envkey_original_env
+    ENV["ENVKEY_ORIGINAL_ENV"] ||= ENV.to_json
+    JSON.parse(ENV["ENVKEY_ORIGINAL_ENV"]).to_h
+  end
 
-  def self.load_env spring_pre_fork_ts=nil,
-                    overload_dotenv_vars=[],
-                    overload_envkey_vars=[]
-    original_env = ENV.to_h
-    dotenv_vars = Dotenv.load
-    updated_dotenv_vars = dotenv_vars.keys.select do |k|
-      overload_dotenv_vars.include?(k) || original_env[k] == nil
-    end
-    overload_dotenv_vars.each do |k|
-      ENV[k] = dotenv_vars[k]
-    end
-
-    if (key = ENV["ENVKEY"])
-      res = Envkey::Fetch.fetch_env(key)
-      if res && res.gsub("\n","").gsub("\r", "") != "" && !res.start_with?("error:")
-        envs = JSON.parse(res)
-        updated_envkey_vars = []
-        envs.each do |k,v|
-          var = k.upcase
-          if !ENV[var] || overload_envkey_vars.include?(var)
-            updated_envkey_vars << var
-            ENV[var] = v
-          end
-        end
-
-        return [Set.new(updated_dotenv_vars), Set.new(updated_envkey_vars)]
-      elsif res.start_with?("error:")
-        STDERR.puts "envkey-fetch " + res
-        raise "ENVKEY invalid. Couldn't load vars."
-      else
-        raise "ENVKEY invalid. Couldn't load vars."
-      end
-    else
+  def self.fetch
+    if !ENV["ENVKEY"]
       raise "ENVKEY missing - must be set as an environment variable or in a gitignored .env file in the root of your project. Go to https://www.envkey.com if you don't know what an ENVKEY is."
+    end
+
+    res = Envkey::Fetch.fetch_env(ENV["ENVKEY"])
+    if res && res.gsub("\n","").gsub("\r", "") != "" && !res.start_with?("error:")
+      return JSON.parse(res)
+    elsif res.start_with?("error:")
+      STDERR.puts "envkey-fetch " + res
+      raise "ENVKEY invalid. Couldn't load vars."
+    else
+      raise "ENVKEY invalid. Couldn't load vars."
     end
   end
 
+  def self.load_env
+    original_env = envkey_original_env
+
+    envs = fetch
+    envs.each do |k,v|
+      var = k.upcase
+      ENV[var] = v if !original_env[var]
+    end
+
+    dotenv_vars = Dotenv.load
+    dotenv_vars.each do |var|
+      ENV[var] = dotenv_vars[var] if !original_env[var]
+    end
+  end
 end
 
--- lib/envkey/rails.rb
+++ lib/envkey/rails.rb
@@ -2,14 +2,12 @@ require "envkey/core"
 
 module Envkey
   class Railtie < Rails::Railtie
-
     config.before_configuration do
       begin
         require "spring/commands"
-        ts = Time.now
-        overload_dotenv_vars, overload_envkey_vars = Envkey::Core.load_env
+        Envkey::Core.load_env
         Spring.after_fork do
-          Envkey::Core.load_env(ts, (overload_dotenv_vars || []), (overload_envkey_vars || []))
+          Envkey::Core.load_env
         end
       rescue LoadError
         Envkey::Core.load_env
